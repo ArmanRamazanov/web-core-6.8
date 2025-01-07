16b177ae856f338751b16df38dbfaf4693edd87a
ArmanRamazanov <arman.ramazanov.04@mail.ru>
2025-01-07 14:22:44 +0500

    Modified

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-//Wanted to have only one main__section-list-button remain active, while keeping the first one active as a default
 const clr_green_500 = "#B8FFEC";
 const clr_green_900 = "#41F6D7";
 const clr_grey_900 = "#7E7E82";
-const clr_black_900 = "#1B1C21"
-const listButtons = document.querySelectorAll(".main__section-list-button");
+const clr_black_900 = "#1B1C21";
 const header__hamburger_menu = document.querySelector(".header__hamburger-menu");
 const header__hamburger_button = document.querySelector(".header__hamburger-button");
 const header__hamburger_menu_button = document.querySelector(".header__hamburger-menu-button");
 const header__hamburger_menu_navigation_list_links = document.querySelectorAll(".header__hamburger-menu-navigation-list-link");
-const header__hamburger_menu_lang_button = document.querySelectorAll(".header__hamburger-menu-lang-button");
+const header__hamburger_menu_lang_buttons = document.querySelectorAll(".header__hamburger-menu-lang-button");
 
 function removeShadow(elements){
     elements.forEach(element=>{
@@ -23,70 +21,40 @@ function removeColor(elements){
     })
 }
 
-listButtons.forEach((button)=>{
-    button.addEventListener("click",event=>{
-        removeShadow(listButtons);
-        let clickedButton = event.target;
-        clickedButton.style.boxShadow = `0 0 0px 3px ${clr_green_500} inset`
-    })
-})
-
 header__hamburger_button.addEventListener("click",()=>{
     header__hamburger_menu.classList.toggle("is-active");
-    const removeFocusOutsideMenu = (selector) =>{
-        
-    const focusableElements = selector.querySelectorAll("a,button")
-    
-    focusableElements.forEach((el)=>{
-        el.setAttribute("tabindex", "-1")
-    })}
-
-    if(header__hamburger_menu.classList.contains("is-active")===true){
-        removeFocusOutsideMenu(document.querySelector(".header"))
-        removeFocusOutsideMenu(document.querySelector(".main"))
-    }    
 })
-
-const removeTabIndex = () => {
-    document.querySelectorAll('[tabindex="-1"]').forEach((element)=>{
-        element.removeAttribute("tabindex")
-    })
-}
 
 header__hamburger_menu_button.addEventListener("click",()=>{
     header__hamburger_menu.classList.toggle("is-active");
-    removeTabIndex()
 })
 
-header__hamburger_menu_navigation_list_links.forEach((link)=>{
+
+//Active Links
+function activeLinkShadow(link){
+    removeShadow(header__hamburger_menu_navigation_list_links)
+    removeColor(header__hamburger_menu_navigation_list_links)
+    link.style.boxShadow  = `5px 0 1px -2px ${clr_green_900} inset`
+    link.style.color = clr_grey_900
+}
+
+const links_active = header__hamburger_menu_navigation_list_links.forEach((link)=>{
     link.addEventListener("click",event=>{
-        removeShadow(header__hamburger_menu_navigation_list_links);
-        removeColor(header__hamburger_menu_navigation_list_links);
-        
-        let clickedLink = event.target;
-
-        clickedLink.style.boxShadow = `5px 0 1px -2px ${clr_green_900} inset`;
-        clickedLink.style.color = clr_grey_900;
+        activeLinkShadow(event.target)
     })
-})
 
-header__hamburger_menu_navigation_list_links.forEach((link)=>{
     link.addEventListener("keypress", event=>{
         if(event.key=" "){
-            removeShadow(header__hamburger_menu_navigation_list_links);
-            removeColor(header__hamburger_menu_navigation_list_links);
-            
-            let clickedLink = event.target;
-    
-            clickedLink.style.boxShadow = `5px 0 1px -2px ${clr_green_900} inset`;
-            clickedLink.style.color = clr_grey_900;
+            activeLinkShadow(event.target)
         }
-    })
-})
+    })})
 
-header__hamburger_menu_lang_button.forEach((button)=>{
+links_active
+
+//Changing the language of the page
+header__hamburger_menu_lang_buttons.forEach((button)=>{
     button.addEventListener("click", event=>{
-        removeColor(header__hamburger_menu_lang_button);
+        removeColor(header__hamburger_menu_lang_buttons);
 
         let langButton = event.target;
 
